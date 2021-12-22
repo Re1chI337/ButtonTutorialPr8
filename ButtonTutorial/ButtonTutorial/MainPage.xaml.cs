7dbe14ce32a277af1ca39c83a43b00a643b861f4
Uzzi Yxung <yxnguzzi@gmail.com>
2021-12-22 23:59:37 +0300

    Добавьте файлы проекта.

---
+++ ButtonTutorial/ButtonTutorial/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ButtonTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int i = 0;
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            i++;
+            (sender as Button).Text = $"{i} Кликов";
+        }
+    }
+}
